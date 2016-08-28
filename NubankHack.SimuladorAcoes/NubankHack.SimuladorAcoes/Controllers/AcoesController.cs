14e1edffa5cce96e86412ae7fd9f93fa9d76f336
Lucas <lucas.segers@hotmail.com>
2016-08-27 21:35:15 -0300

    Projetos dados e acesso

---
+++ NubankHack.SimuladorAcoes/NubankHack.SimuladorAcoes/Controllers/AcoesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NubankHack.SimuladorAcoes.Controllers
+{
+    public class AcoesController : ApiController
+    {
+        [HttpGet]
+        [Route("Acoes/Teste")]
+        public string Teste()
+        {
+            return "OK";
+        }
+
+    }
+}
